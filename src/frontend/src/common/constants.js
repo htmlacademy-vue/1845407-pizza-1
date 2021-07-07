68aac229aa75cd9837a6402be3c68ffb577d5a4a
m.danuschenkov <m.danuschenkov@ctm.ru>
2021-07-07 11:58:00 +0300

    FIX дополнение компонентов пиццы типами https://github.com/htmlacademy-vue/1845407-pizza-1/pull/1#discussion_r664918802

--- src/frontend/src/common/constants.js
+++ src/frontend/src/common/constants.js
@@ -1,33 +1,99 @@
-export const PIZZA_DOUGH_TYPES = {
-  Тонкое: "light",
-  Толстое: "large",
-};
+export const PIZZA_DOUGH_TYPES = [
+  {
+    name: "Тонкое",
+    type: "light",
+  },
+  {
+    name: "Толстое",
+    type: "large",
+  },
+];
 
-export const PIZZA_INGREDIENTS_TYPES = {
-  Грибы: "mushrooms",
-  Чеддер: "cheddar",
-  Салями: "salami",
-  Ветчина: "ham",
-  Ананас: "ananas",
-  Бекон: "bacon",
-  Лук: "onion",
-  Чили: "chile",
-  Халапеньо: "jalapeno",
-  Маслины: "olives",
-  Томаты: "tomatoes",
-  Лосось: "salmon",
-  Моцарелла: "mozzarella",
-  Пармезан: "parmesan",
-  "Блю чиз": "blue_cheese",
-};
+export const PIZZA_INGREDIENTS_TYPES = [
+  {
+    name: "Грибы",
+    type: "mushrooms",
+  },
+  {
+    name: "Чеддер",
+    type: "cheddar",
+  },
+  {
+    name: "Салями",
+    type: "salami",
+  },
+  {
+    name: "Ветчина",
+    type: "ham",
+  },
+  {
+    name: "Ананас",
+    type: "ananas",
+  },
+  {
+    name: "Бекон",
+    type: "bacon",
+  },
+  {
+    name: "Лук",
+    type: "onion",
+  },
+  {
+    name: "Чили",
+    type: "chile",
+  },
+  {
+    name: "Халапеньо",
+    type: "jalapeno",
+  },
+  {
+    name: "Маслины",
+    type: "olives",
+  },
+  {
+    name: "Томаты",
+    type: "tomatoes",
+  },
+  {
+    name: "Лосось",
+    type: "salmon",
+  },
+  {
+    name: "Моцарелла",
+    type: "mozzarella",
+  },
+  {
+    name: "Пармезан",
+    type: "parmesan",
+  },
+  {
+    name: "Блю чиз",
+    type: "blue_cheese",
+  },
+];
 
-export const PIZZA_SAUCES_TYPES = {
-  Томатный: "tomato",
-  Сливочный: "creamy",
-};
+export const PIZZA_SAUCES_TYPES = [
+  {
+    name: "Томатный",
+    type: "tomato",
+  },
+  {
+    name: "Сливочный",
+    type: "creamy",
+  },
+];
 
-export const PIZZA_SIZES_TYPES = {
-  "23 см": "small",
-  "32 см": "normal",
-  "45 см": "large",
-};
+export const PIZZA_SIZES_TYPES = [
+  {
+    name: "23 см",
+    type: "small",
+  },
+  {
+    name: "32 см",
+    type: "normal",
+  },
+  {
+    name: "45 см",
+    type: "large",
+  },
+];
