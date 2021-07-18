f1f9505f08e4b515e2e7e2678c605ee8c1bdc62c
m.danuschenkov <m.danuschenkov@ctm.ru>
2021-07-19 00:08:23 +0300

    FEAT CartFooter вывод итога в футере

--- src/frontend/src/modules/cart/components/CartFooter.vue
+++ src/frontend/src/modules/cart/components/CartFooter.vue
@@ -1,25 +1,43 @@
 <template>
   <section class="footer">
     <div class="footer__more">
-      <a href="#" class="button button--border button--arrow">
+      <router-link
+        :to="{ name: 'builder' }"
+        class="button button--border button--arrow"
+      >
         Хочу еще одну
-      </a>
+      </router-link>
     </div>
     <p class="footer__text">
       Перейти к конструктору<br />чтоб собрать ещё одну пиццу
     </p>
     <div class="footer__price">
-      <b>Итого: 2 228 ₽</b>
+      <b>Итого: {{ price }} ₽</b>
     </div>
 
     <div class="footer__submit">
-      <button type="submit" class="button">Оформить заказ</button>
+      <button
+        type="submit"
+        class="button"
+        :class="{ 'button--disabled': !ready }"
+        :disabled="!ready"
+      >
+        Оформить заказ
+      </button>
     </div>
   </section>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "CartFooter",
+  computed: {
+    ...mapGetters("Cart", ["price"]),
+    ready() {
+      return !!this.price;
+    },
+  },
 };
 </script>
