17d1ed1988b3b7377433d94fb7bfe6f1e8ea1508
m.danuschenkov <m.danuschenkov@ctm.ru>
2021-10-11 17:17:26 +0300

    FEAT test Layouts странное пустое тестирование

---
+++ src/frontend/src/layouts/__tests__/Default.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+import DefaultLayout from "@/layouts/Default";
+
+const stubs = ["router-view"];
+
+describe("DefaultLayout", () => {
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = shallowMount(DefaultLayout, options);
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("is rendered", () => {
+    createComponent({ stubs });
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
